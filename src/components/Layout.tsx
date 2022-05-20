dd63993dd8f51df126cbfe543031ce80605dd8ab
Hironao OTSUBO <motemen@gmail.com>
2022-05-20 11:36:05 +0900

    add react-helmet

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { Link } from "gatsby";
 import * as React from "react";
+import { Helmet } from "react-helmet";
 
 interface Props extends React.PropsWithChildren<{}> {
   pageTitle?: string;
@@ -8,17 +9,19 @@ interface Props extends React.PropsWithChildren<{}> {
 const Layout = ({ pageTitle, children }: Props) => {
   return (
     <main>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="anonymous"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;500&display=swap"
-        rel="stylesheet"
-      />
-      <title>{pageTitle ? `${pageTitle} | ` : ""}motemen</title>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;500&display=swap"
+          rel="stylesheet"
+        />
+        <title>{pageTitle ? `${pageTitle} | ` : ""}motemen</title>
+      </Helmet>
       <h1>
         <Link to="/">motemen</Link>
       </h1>
